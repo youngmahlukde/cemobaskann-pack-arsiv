4c8f1feb8d316bff5e3105c1fad358f4495fb19f
mahluk atkas <106095586+youngmahlukde@users.noreply.github.com>
2022-05-23 20:02:20 +0300

    Update webhook.lua

--- discordlog/webhook.lua
+++ discordlog/webhook.lua
@@ -1,6 +1,6 @@
-﻿-- Client -- Discord Sunucunuz > Ayarlar > WebHooks Oluştur
+﻿-- mahluk yaydı
 webhooks = {
 	-- isim, isim, webhook linki, avatar url (bot resmi)
-	{"Komutlog", "Komutlog", "https://discord.com/api/webhooks/973548671083876432/qDKjzaQ7AfYBoLfPKPQrYl_tCBPwO-vDA9ZHqZ1Ei6dh59TDwBekCsp3BrzmtEfX4wOI", "https://media.discordapp.net/attachments/824219555475619861/933678184203960390/client-scripting.png"},
+	{"Komutlog", "Komutlog", "weebhook linki", "weebhook avatar için resim"},
 
 }
